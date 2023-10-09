44eec5e58a5ee69eb997b152ee5d84f5fb8126c0
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-09 17:32:01 +0300

    added test for getting popular repositories by languages properly

---
+++ src/routes/HomeRoute.test.js
@@ -0,0 +1,63 @@
+import { screen, render } from "@testing-library/react";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import { MemoryRouter } from "react-router-dom";
+import HomeRoute from "./HomeRoute";
+
+const handlers = [
+  rest.get("/api/repositories", (req, res, ctx) => {
+    const language = req.url.searchParams.get("q").split("language:")[1];
+
+    return res(
+      ctx.json({
+        items: [
+          { id: 1, full_name: `${language}_one` },
+          { id: 2, full_name: `${language}_two` },
+        ],
+      })
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
+
+test("renders two links for each language", async () => {
+  render(
+    <MemoryRouter>
+      <HomeRoute />
+    </MemoryRouter>
+  );
+
+  const languages = [
+    "javascript",
+    "typescript",
+    "rust",
+    "go",
+    "python",
+    "java",
+  ];
+
+  for (let language of languages) {
+    const links = await screen.findAllByRole("link", {
+      name: new RegExp(`${language}_`),
+    });
+
+    expect(links).toHaveLength(2);
+    expect(links[0]).toHaveTextContent(`${language}_one`);
+    expect(links[1]).toHaveTextContent(`${language}_two`);
+    expect(links[0]).toHaveAttribute("href", `/repositories/${language}_one`);
+    expect(links[1]).toHaveAttribute("href", `/repositories/${language}_two`);
+  }
+});
