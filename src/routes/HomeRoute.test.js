22875d0a622afd4e2598c29872859d19776b3a13
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-09 17:46:25 +0300

    made separate createServer function to use handlers as reusable

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -1,37 +1,23 @@
 import { screen, render } from "@testing-library/react";
-import { setupServer } from "msw/node";
-import { rest } from "msw";
 import { MemoryRouter } from "react-router-dom";
 import HomeRoute from "./HomeRoute";
+import { createServer } from "../test/server";
 
-const handlers = [
-  rest.get("/api/repositories", (req, res, ctx) => {
-    const language = req.url.searchParams.get("q").split("language:")[1];
+createServer([
+  {
+    path: "/api/repositories",
+    res: (req) => {
+      const language = req.url.searchParams.get("q").split("language:")[1];
 
-    return res(
-      ctx.json({
+      return {
         items: [
           { id: 1, full_name: `${language}_one` },
           { id: 2, full_name: `${language}_two` },
         ],
-      })
-    );
-  }),
-];
-
-const server = setupServer(...handlers);
-
-beforeAll(() => {
-  server.listen();
-});
-
-afterEach(() => {
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  server.close();
-});
+      };
+    },
+  },
+]);
 
 test("renders two links for each language", async () => {
   render(
